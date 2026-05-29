0c157b0d2c8f240d3684a58fc2105a9e6e17e4c0
KanishkaArochana <kanishkaarochana23@gmail.com>
2026-05-29 20:13:59 +0530

    Add the CRUD oparations
    Create CRUD operation

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -1,19 +1,115 @@
+// var express = require("express");
+// var app = express();
+
+// // Home route
+// app.get("/", function (req, res) {
+//   res.send('{ "response": "Hello, Welcome to Adomic Arts samin" }');
+// });
+
+// // Test route
+// app.get("/will", function (req, res) {
+//   res.send('{ "response": "Hello World" }');
+// });
+
+// // Ready route
+// app.get("/ready", function (req, res) {
+//   res.send('{ "response": "Great!, It works!" }');
+// });
+
+// module.exports = app;
+
+
+
 var express = require("express");
 var app = express();
 
-// Home route
-app.get("/", function (req, res) {
-  res.send('{ "response": "Hello, Welcome to Adomic Arts samin" }');
+app.use(express.json());
+
+
+// Default 5 users
+let users = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Samin" },
+    { id: 3, name: "Alex" },
+    { id: 4, name: "Kamal" },
+    { id: 5, name: "Nimal" }
+];
+
+
+// CREATE USER
+app.post("/users", function (req, res) {
+
+    const user = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+
+    users.push(user);
+
+    res.status(201).json(user);
+
 });
 
-// Test route
-app.get("/will", function (req, res) {
-  res.send('{ "response": "Hello World" }');
+
+// READ ALL USERS
+app.get("/users", function (req, res) {
+
+    res.json(users);
+
 });
 
-// Ready route
-app.get("/ready", function (req, res) {
-  res.send('{ "response": "Great!, It works!" }');
+
+// READ SINGLE USER
+app.get("/users/:id", function (req, res) {
+
+    const user = users.find(u => u.id == req.params.id);
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    res.json(user);
+
+});
+
+
+// UPDATE USER
+app.put("/users/:id", function (req, res) {
+
+    const user = users.find(u => u.id == req.params.id);
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    user.name = req.body.name;
+
+    res.json(user);
+
+});
+
+
+// DELETE USER
+app.delete("/users/:id", function (req, res) {
+
+    const user = users.find(u => u.id == req.params.id);
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    users = users.filter(u => u.id != req.params.id);
+
+    res.json({
+        message: "User deleted"
+    });
+
 });
 
 module.exports = app;
--- nodeApp/test/test.js
+++ nodeApp/test/test.js
@@ -1,14 +1,118 @@
-var request = require('supertest');
-var app = require('../app');
+// var request = require('supertest');
+// var app = require('../app');
 
-describe('GET /will', function () {
+// describe('GET /will', function () {
 
-    it('respond with hello world', function (done) {
+//     it('respond with hello world', function (done) {
+
+//         request(app)
+//             .get('/will')
+//             .expect(200)
+//             .expect('{ "response": "Hello World" }', done);
+
+//     });
+
+// });
+
+
+
+var request = require("supertest");
+var app = require("../app");
+
+describe("CRUD Operations Test", function () {
+
+    // GET ALL USERS
+    it("GET /users", function (done) {
 
         request(app)
-            .get('/will')
+            .get("/users")
             .expect(200)
-            .expect('{ "response": "Hello World" }', done);
+            .expect(function (res) {
+
+                if (res.body.length < 5) {
+                    throw new Error("Users not found");
+                }
+
+            })
+            .end(done);
+
+    });
+
+
+    // GET SINGLE USER
+    it("GET /users/1", function (done) {
+
+        request(app)
+            .get("/users/1")
+            .expect(200)
+            .expect(function (res) {
+
+                if (res.body.name !== "John") {
+                    throw new Error("Wrong user");
+                }
+
+            })
+            .end(done);
+
+    });
+
+
+    // CREATE USER
+    it("POST /users", function (done) {
+
+        request(app)
+            .post("/users")
+            .send({
+                name: "Kasun"
+            })
+            .expect(201)
+            .expect(function (res) {
+
+                if (res.body.name !== "Kasun") {
+                    throw new Error("User not created");
+                }
+
+            })
+            .end(done);
+
+    });
+
+
+    // UPDATE USER
+    it("PUT /users/2", function (done) {
+
+        request(app)
+            .put("/users/2")
+            .send({
+                name: "Updated Samin"
+            })
+            .expect(200)
+            .expect(function (res) {
+
+                if (res.body.name !== "Updated Samin") {
+                    throw new Error("User not updated");
+                }
+
+            })
+            .end(done);
+
+    });
+
+
+    // DELETE USER
+    it("DELETE /users/3", function (done) {
+
+        request(app)
+            .delete("/users/3")
+            .expect(200)
+            .expect(function (res) {
+
+                if (res.body.message !== "User deleted") {
+                    throw new Error("User not deleted");
+                }
+
+            })
+            .end(done);
 
     });
 
